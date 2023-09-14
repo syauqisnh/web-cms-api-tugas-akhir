d185550e98e9cf6b76ff5ece3937f2e9b2ce7200
Syauqi <syauqisnh@gmail.com>
2023-09-14 21:37:32 +0700

    delete: file index.js

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-
-router.get('/employee', (req, res) => {
-    res.json({ message: 'Berhasil Menggunakan Router' });
-});
-
-export default router;
