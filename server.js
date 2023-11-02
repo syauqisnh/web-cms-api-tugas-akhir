7df0150663ef7c0b9d328aef2560634b5a0082e2
Syauqi <syauqisnh@gmail.com>
2023-11-02 09:36:21 +0700

    add: .env

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
+require('dotenv').config();
 const express = require('express');
 const route = require('./routes/route');
 const cors = require('cors'); // Import middleware cors
 const app = express();
 
 app.use(express.json());
-
 // Gunakan middleware cors dengan opsi yang sesuai
 app.use(cors());
-
 app.use(route);
 
 app.listen(9900, () => {
