947a1f945007880f8bf6ca3fad1d371f765e95b5
Syauqi <syauqisnh@gmail.com>
2024-03-04 01:10:04 +0700

    modify: router_customer

--- routes/r_customer.js
+++ routes/r_customer.js
@@ -15,7 +15,7 @@ router.post('/customer',authenticate, adminOnly, post_customer);
 router.put('/customer/:customer_uuid',authenticate, put_customer);
 router.delete('/customer/:customer_uuid', authenticate, adminOnly, delete_customer);
 router.get('/customer/get_all',authenticate, adminOnly, get_all_customer);
-router.get('/customer/get_uniqe', authenticate, adminOnly, get_uniqe_customer);
+router.get('/customer/get_uniqe', authenticate, get_uniqe_customer);
 router.get('/customer/get_count', authenticate, adminOnly, get_count_customer);
 router.get('/customer/:customer_uuid',authenticate, get_detail_customer);
 
