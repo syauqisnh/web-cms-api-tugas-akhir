50df6da82234efcb008624bd23ba2118e4c01c64
Syauqi <syauqisnh@gmail.com>
2024-03-08 02:09:01 +0700

    delete comment

--- routes/r_user.js
+++ routes/r_user.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { authenticate, adminOnly } = require('../middleware/authMiddleware'); // Impor middleware
+const { authenticate, adminOnly } = require('../middleware/authMiddleware');
 
 const {
     post_user,
@@ -11,12 +11,12 @@ const {
     get_count_user,
 } = require('../controllers/c_user');
 
-router.post('/user',authenticate, adminOnly, post_user); // Rute ini tidak dilindungi, misalnya untuk pendaftaran
-router.put('/user/:user_uuid', authenticate, adminOnly, put_user); // Rute ini dilindungi
-router.delete('/user/:user_uuid', authenticate, adminOnly, delete_user); // Rute ini dilindungi
-router.get('/user/get_all',authenticate, adminOnly, get_all_user); // Rute ini dilindungi
-router.get('/user/get_uniqe', authenticate, adminOnly, get_uniqe_user); // Rute ini dilindungi
-router.get('/user/get_count', authenticate, adminOnly, get_count_user); // Rute ini dilindungi
-router.get('/user/:user_uuid', authenticate, adminOnly, get_detail_user); // Rute ini dilindungi
+router.post('/user',authenticate, adminOnly, post_user); 
+router.put('/user/:user_uuid', authenticate, adminOnly, put_user); 
+router.delete('/user/:user_uuid', authenticate, adminOnly, delete_user); 
+router.get('/user/get_all',authenticate, adminOnly, get_all_user);
+router.get('/user/get_uniqe', authenticate, adminOnly, get_uniqe_user);
+router.get('/user/get_count', authenticate, adminOnly, get_count_user);
+router.get('/user/:user_uuid', authenticate, adminOnly, get_detail_user);
 
 module.exports = router;
