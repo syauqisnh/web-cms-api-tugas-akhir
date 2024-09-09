70f733561f60e9357d7a3f1048a1b8b1ded6c0c4
Syauqi <syauqisnh@gmail.com>
2024-09-09 18:21:03 +0700

    add: new seaderr

--- seeders/20231204024044-demo-tbl_user.js
+++ seeders/20231204024044-demo-tbl_user.js
@@ -4,7 +4,7 @@ const saltRounds = 10;
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    const hashedPassword = await bcrypt.hash('Bekantan@bjm', saltRounds);
+    const hashedPassword = await bcrypt.hash('superadmin', saltRounds);
     const level = await queryInterface.sequelize.query(
       `SELECT level_uuid FROM tbl_levels WHERE level_name = 'super administrator';`, 
       { type: queryInterface.sequelize.QueryTypes.SELECT }
@@ -14,18 +14,18 @@ module.exports = {
       throw new Error('Level super administrator tidak ditemukan');
     }
 
-    const superAdminUuid = level[0].level_uuid; // Perbaikan di sini
+    const superAdminUuid = level[0].level_uuid; 
 
     return queryInterface.bulkInsert('tbl_user', [
       {
         user_uuid: 'c2119049-d099-456a-8597-6b06eec9d9b3',
-        user_username: 'bekantan_administrator',
-        user_full_name: 'bekantan administrator',
+        user_username: 'super administrator',
+        user_full_name: 'super administrator',
         user_nohp: '',
-        user_email: 'superadmin@gmail.com',
+        user_email: 'admins@gmail.com',
         user_address: '',
         user_password: hashedPassword,
-        user_level: superAdminUuid, // Gunakan UUID dari tbl_levels
+        user_level: superAdminUuid, 
         user_create_at: new Date(),
         user_update_at: null,
         user_delete_at: null,
